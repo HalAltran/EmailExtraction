20ed9616d58f8d6071afd2da1bb8f8c318e7e0d7
hsmith <hal.smith@altran.com>
2020-09-15 11:37:50 +0100

    Finds email addresses on a web page and counts number of domain occurrences.

--- email_extraction/DocumentContainingEmails.py
+++ email_extraction/DocumentContainingEmails.py
@@ -1,36 +1,30 @@
+import urllib.request as urllib
+import re
+
+
+def get_web_content(url: str):
+    content = ""
+    try:
+        web_page = urllib.urlopen(url)
+        content = web_page.read()
+    except:
+        print("An error occurred reading the web page %s" % web_page)
+    return str(content)
+
+
 class DocumentContainingEmails:
     def __init__(self, file_path):
-        with open(file_path) as file:
-            self.content = file.read()
-            self.email_dictionary = {}
-            self.create_email_dictionary()
-
-    def get_words(self):
-        return self.content.split(" ")
-
-    def get_email_addresses(self):
-        list_of_words = self.get_words()
-        email_addresses = []
-        for word in list_of_words:
-            if word.__contains__("@"):
-                email_addresses.append(word)
-        return email_addresses
-
-    def get_softwire_email_address_count(self):
-        email_addresses = self.get_email_addresses()
-        count = 0
-        for email_address in email_addresses:
-            if email_address.endswith("@softwire.com"):
-                count += 1
-        return count
+        self.email_dictionary = {}
+        self.content = get_web_content(file_path)
+        self.create_email_dictionary()
 
     def create_email_dictionary(self):
-        list_of_words = self.get_words()
-        for word in list_of_words:
-            if word.__contains__("@"):
+        list_of_emails = re.findall(r"\S+@\S+", self.content)
+        for email in list_of_emails:
+            email_suffix_list = re.findall(r"@\w+.", email)
+            if len(email_suffix_list) > 0:
                 count = 1
-                email_suffix = word.split("@")[1]
-                email_suffix = email_suffix.split(".")[0]
+                email_suffix = email_suffix_list[0]
                 if email_suffix in self.email_dictionary:
                     count = self.email_dictionary[email_suffix] + 1
                 self.email_dictionary[email_suffix] = count
--- email_extraction/__init__.py
+++ email_extraction/__init__.py
@@ -1,7 +1,9 @@
 from email_extraction.DocumentContainingEmails import DocumentContainingEmails
 
 if __name__ == "__main__":
-    doc = DocumentContainingEmails("../res/sample.txt")
+    # web_url = "http://free-email-database.blogspot.com/2008/12/welcome-to-free-e-mail-database.html"
+    web_url = input("Enter url of web page to extract emails from: ")
+    doc = DocumentContainingEmails(web_url)
     entry_count_min = input("Display email domains that occur more than this number of times: ")
     doc.rank_and_remove(entry_count_min)
     print(doc.email_dictionary)
